1f147d38e047f3cb68268a797ccf01e45869d897
Travis Geers <armyman1992@hotmail.com>
2022-12-07 17:54:23 -0700

    defined models, added api routes, and connected to sequelize

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,72 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
+// find all categories
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    // include its associated Products
+    include: [Product]
+  })
+  .then(categories => res.json(categories))
+  .catch(err => res.status(500).json(err));
 });
 
+// find one category by its `id` value
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Product]
+  })
+  .then(category => {
+    // if category doesn't exist, return 404 error
+    if(!category) {
+      res.status(404).json({ message: 'No existing category with this given ID.' });
+      return;
+    }
+    res.json(category);
+  })
+  .catch(err => res.status(500).json(err));
 });
 
+// create a new category
 router.post('/', (req, res) => {
-  // create a new category
+  Category.create(req.body)
+  .then(newCategory => res.json(newCategory))
+  .catch(err => res.status(500).json(err));
 });
 
+// update a category by its `id` value
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(updatedCategory => {
+    if(!updatedCategory) {
+      res.status(404).json({ message: 'No existing category with this given ID.' })
+    }
+    res.json(updatedCategory);
+  })
+  .catch(err => res.status(500).json(err));
 });
 
+// delete a category 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(category => {
+    if(!category) {
+      res.status(404).json({ message: 'No existing category with this given ID.' })
+    }
+    res.json(category);
+  })
+  .catch(err => res.status(500).json(err));
 });
 
 module.exports = router;
